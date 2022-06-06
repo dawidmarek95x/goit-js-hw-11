50d164f46d0ba3f7254cd2d54d2cc615d6848c93
dawidmarek95x <dawid.marek95x@gmail.com>
2022-06-06 19:18:44 +0200

    update/fetchImages
    Added fetchImages.js module containing a function that sends a query to the pixabay.com server to retrieve an object with images data.

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,3 +1,24 @@
 // Import of Axios library
 import axios from "axios";
 
+// Function that sends a request to the pixabay.com server
+export async function fetchImages(searchedValue, page) {
+  const params = new URLSearchParams({
+    key: "27887941-26f96d7878e5748cf06133d38",
+    q: searchedValue,
+    image_type: "photo",
+    orientation: "horizontal",
+    safesearch: true,
+    per_page: 40,
+    page,
+  });
+
+  await axios({
+    method: 'get',
+    url: `https://pixabay.com/api/?${params}`,
+  })
+  .then((response) => {
+    console.log(response.data);
+    return response.data;
+  });
+};
